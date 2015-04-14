74a45c90dd9f70f62cf48331487242569181212c
Roseanna Chu <Riven@roseannas-air.bc.edu>
2015-04-14 16:33:00 -0400

    Initial Commit

---
+++ PeaceCorps Openings/PeaceCorps Openings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PeaceCorps Openings
+//
+//  Created by Roseanna Chu on 4/14/15.
+//  Copyright (c) 2015 GAI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
