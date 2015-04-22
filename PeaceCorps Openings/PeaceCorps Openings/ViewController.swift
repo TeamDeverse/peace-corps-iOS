174797bf200c25172492223917c920eb0fb0f16b
Roseanna Chu <Riven@roseannas-air.bc.edu>
2015-04-22 19:17:24 -0400

    for printing

--- PeaceCorps Openings/PeaceCorps Openings/ViewController.swift
+++ PeaceCorps Openings/PeaceCorps Openings/ViewController.swift
@@ -64,6 +64,7 @@ class ViewController: UIViewController {
         var err: NSError
         // throwing an error on the line below (can't figure out where the error message is)
         var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+        iterate(jsonResult)
     }
 
     func iterate(jsonResults: NSDictionary){
@@ -79,6 +80,17 @@ class ViewController: UIViewController {
             }
         }
 
+    }
+    func insert(jsonResults: NSDictionary){
+        let dataArray = jsonResults["results"] as NSArray;
+        println("Data items count: \(dataArray.count)")
+        for item in dataArray { // loop through data items
+            let obj = item as NSDictionary
+            for (key, value) in obj {
+                    println("Property: \(key as String)")
+                    println("Value: \"\(value)\"")
+            }
+        }
     }
     
     func filter(userInput: String, jsonResults: NSDictionary){
