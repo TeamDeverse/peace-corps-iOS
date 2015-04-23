b0e78ac0f8cb767918a483841a0c4a8343c7ce5a
Roseanna Chu <Riven@roseannas-air.bc.edu>
2015-04-22 21:26:16 -0400

    this is bad

--- PeaceCorps Openings/PeaceCorps Openings/ViewController.swift
+++ PeaceCorps Openings/PeaceCorps Openings/ViewController.swift
@@ -9,31 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-    lazy var data = NSMutableData()
+//    lazy var data = NSMutableData()
     
-    struct Job{
-        let title   : String
-        let req_id  : String
-        let country : String
-        let region  : String
-        let sector  : String
-        let appy_date   : String
-        let know_by : String
-        let staging_start_date  : String
-        let featured    : Boolean
-        let project_description : String
-        let required_skills : String
-        let desired_skills  : String
-        let language_skills : String
-        let language_skills_comments    : String
-        let volunteers_requested    : Int
-        let accepts_couples : Boolean
-        let living_conditions_comments  : String
-        let country_medical_considerations  : String
-        let country_site_url    : String
-        let country_flag_image  : String
-        let opening_url : String
-    }
+//    struct Job{
+//        let title   : String
+//        let req_id  : String
+//        let country : String
+//        let region  : String
+//        let sector  : String
+//        let appy_date   : String
+//        let know_by : String
+//        let staging_start_date  : String
+//        let featured    : Boolean
+//        let project_description : String
+//        let required_skills : String
+//        let desired_skills  : String
+//        let language_skills : String
+//        let language_skills_comments    : String
+//        let volunteers_requested    : Int
+//        let accepts_couples : Boolean
+//        let living_conditions_comments  : String
+//        let country_medical_considerations  : String
+//        let country_site_url    : String
+//        let country_flag_image  : String
+//        let opening_url : String
+//    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,69 +41,69 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        startConnection()
+//        startConnection()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    func startConnection(){
-        let urlPath: String = "http://www.peacecorps.gov/api/v1/openings/?keyword=teacher"
-        var url: NSURL = NSURL(string: urlPath)!
-        var request: NSURLRequest = NSURLRequest(URL: url)
-        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)!
-        connection.start()
-    }
-    
-    func connection(connection: NSURLConnection!, didReceiveData data: NSData!){
-        self.data.appendData(data)
-    }
-
-    func connectionDidFinishLoading(connection: NSURLConnection!) {
-        var err: NSError
-        // throwing an error on the line below (can't figure out where the error message is)
-        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
-        iterate(jsonResult)
-    }
-
-    func iterate(jsonResults: NSDictionary){
-        let dataArray = jsonResults["results"] as NSArray;
-        
-        println("Data items count: \(dataArray.count)")
-        
-        for item in dataArray { // loop through data items
-            let obj = item as NSDictionary
-            for (key, value) in obj {
-                println("Property: \(key as String)")
-                println("Value: \"\(value)\"")
-            }
-        }
-
-    }
-    func insert(jsonResults: NSDictionary){
-        let dataArray = jsonResults["results"] as NSArray;
-        println("Data items count: \(dataArray.count)")
-        for item in dataArray { // loop through data items
-            let obj = item as NSDictionary
-            for (key, value) in obj {
-                    println("Property: \(key as String)")
-                    println("Value: \"\(value)\"")
-            }
-        }
-    }
-    
-    func filter(userInput: String, jsonResults: NSDictionary){
-        let dataArray = jsonResults["results"] as NSArray;
-        println("Data items count: \(dataArray.count)")
-        for item in dataArray { // loop through data items
-            let obj = item as NSDictionary
-            for (key, value) in obj {
-                if(key as NSString == userInput){
-                    println("Property: \(key as String)")
-                    println("Value: \"\(value)\"")
-                }
-            }
-        }
-    }
+//    func startConnection(){
+//        let urlPath: String = "http://www.peacecorps.gov/api/v1/openings/?keyword=teacher"
+//        var url: NSURL = NSURL(string: urlPath)!
+//        var request: NSURLRequest = NSURLRequest(URL: url)
+//        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)!
+//        connection.start()
+//    }
+//    
+//    func connection(connection: NSURLConnection!, didReceiveData data: NSData!){
+//        self.data.appendData(data)
+//    }
+//
+//    func connectionDidFinishLoading(connection: NSURLConnection!) {
+//        var err: NSError
+//        // throwing an error on the line below (can't figure out where the error message is)
+//        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+//        //iterate(jsonResult)
+//    }
+//
+//    func iterate(jsonResults: NSDictionary){
+//        let dataArray = jsonResults["results"] as NSArray;
+//        
+//        println("Data items count: \(dataArray.count)")
+//        
+//        for item in dataArray { // loop through data items
+//            let obj = item as NSDictionary
+//            for (key, value) in obj {
+//                println("Property: \(key as String)")
+//                println("Value: \"\(value)\"")
+//            }
+//        }
+//
+//    }
+//    func insert(jsonResults: NSDictionary){
+//        let dataArray = jsonResults["results"] as NSArray;
+//        println("Data items count: \(dataArray.count)")
+//        for item in dataArray { // loop through data items
+//            let obj = item as NSDictionary
+//            for (key, value) in obj {
+//                    println("Property: \(key as String)")
+//                    println("Value: \"\(value)\"")
+//            }
+//        }
+//    }
+//    
+//    func filter(userInput: String, jsonResults: NSDictionary){
+//        let dataArray = jsonResults["results"] as NSArray;
+//        println("Data items count: \(dataArray.count)")
+//        for item in dataArray { // loop through data items
+//            let obj = item as NSDictionary
+//            for (key, value) in obj {
+//                if(key as NSString == userInput){
+//                    println("Property: \(key as String)")
+//                    println("Value: \"\(value)\"")
+//                }
+//            }
+//        }
+//    }
 }
